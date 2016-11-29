a4b8ce62f155625b1a6e388dca17a29b034b7ba8
keomony khun <keomony@gmail.com>
2016-11-29 16:31:36 +0000

    adds new link

--- spec/features/creating_link_spec.rb
+++ spec/features/creating_link_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+RSpec.feature 'Link add new' do
+  scenario 'we want to add the website address and title to our bookmark manager' do
+
+    visit '/links/new'
+    expect(page.status_code).to eq 200
+    
+    fill_in 'title', :with => 'Github Pong'
+    fill_in 'url', :with => 'https://github.com/makersacademy/course/blob/master/pills/github_pong.md'
+    click_button "Add"
+    expect(page.current_path).to eq "/links"
+
+    within 'ul#links' do
+      expect(page).to have_content("Github Pong")
+    end
+  end
+
+end
