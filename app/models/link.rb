a2e65b2507d8a05b2ae35f634f0dc21b652ded5a
George Eaton <george_eaton@hotmail.co.uk>
2016-11-30 10:24:23 +0000

    moved datamapper debug

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link
   property :url, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
 DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
 DataMapper.finalize
 DataMapper.auto_upgrade!
