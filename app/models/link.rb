c3f8e3dab171473523d57b8c54c524785b797201
keomony khun <keomony@gmail.com>
2016-11-30 11:56:51 +0000

    uses rack environment to connect to different databases in dev and test

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link
   property :url, String
 
   DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,2 @@
-require 'rubygems'
-require File.join(File.dirname(__FILE__), 'app.rb')
-
-run bookmark_manager
+require './app/app.rb'
+run BookmarkManager
