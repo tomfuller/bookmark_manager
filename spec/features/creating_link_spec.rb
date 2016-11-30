fc69dfe453ebf286c770a04d5393fe300e7358a7
keomony khun <keomony@gmail.com>
2016-11-30 10:49:23 +0000

    features test for add link via new link form

--- spec/features/creating_link_spec.rb
+++ spec/features/creating_link_spec.rb
@@ -4,16 +4,10 @@ RSpec.feature 'Link add new' do
   scenario 'we want to add the website address and title to our bookmark manager' do
 
     visit '/links/new'
-    expect(page.status_code).to eq 200
-    
-    fill_in 'title', :with => 'Github Pong'
-    fill_in 'url', :with => 'https://github.com/makersacademy/course/blob/master/pills/github_pong.md'
-    click_button "Add"
-    expect(page.current_path).to eq "/links"
-
-    within 'ul#links' do
-      expect(page).to have_content("Github Pong")
-    end
+    fill_in 'title', :with => 'BBC news'
+    fill_in 'url', :with => 'http://www.bbc.co.uk/'
+    click_button('Add')
+    expect(page).to have_content('BBC news')
   end
 
 end
