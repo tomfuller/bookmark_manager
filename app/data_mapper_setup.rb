f1bd843439cc0a6c30e3850f14ecede12cfb6009
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-01 10:55:36 +0000

    remove debugging output in tests, it's messy

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'dm-postgres-adapter'
 require_relative 'models/link'
 require_relative 'models/tag'
 
-DataMapper::Logger.new($stdout, :debug)
+# DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
